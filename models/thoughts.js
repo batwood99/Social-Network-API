1c454415055e93216d3af9e0f2f5d4c0cef04d1c
batwood99 <atwood@mail.usf.edu>
2023-07-29 10:09:44 -0400

    started on thoughts model

---
+++ models/thoughts.js
@@ -0,0 +1,31 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/formatDate.js');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatter => dateFormat(formatter)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
