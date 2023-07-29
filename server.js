fa9d6e931dcb7b3cd87c5f588d21c1e377010d40
batwood99 <atwood@mail.usf.edu>
2023-07-29 09:58:02 -0400

    added code for server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./connection')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(require('./routes'));
+
+mongoose.set('debug', true);
+
+db.once("open", ()=>{
+
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+})
