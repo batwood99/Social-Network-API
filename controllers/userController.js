71cf1b34f6dc2d22bc55e292ac70729611405fa8
batwood99 <atwood@mail.usf.edu>
2023-07-29 13:11:48 -0400

    started on user controller

---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const {User} = require('../models');
+
+
+const userController = {
+
+
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+getUsersById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+createUser({ body }, res) {
+    User.create(body)
+      .then(dbuserData => res.json(dbuserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+
